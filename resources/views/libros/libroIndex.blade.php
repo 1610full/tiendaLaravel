4c0005365109a7dfe1d5fd07a27a4abb62270675
Alan Zaragoza <a_j_z_p@hotmail.com>
2022-10-26 16:46:34 -0500

    autores 1 a M, proveedores M a M

--- resources/views/libros/libroIndex.blade.php
+++ resources/views/libros/libroIndex.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Index libros</title>
+
+    @vite(['resources/css/materialize.css', 'resources/js/materialize.js'])
 </head>
 <body>
     <h1>Libros</h1>
